0a505842a1b57130d49aa8cd64bf8ea3fbf094cd
Jerlyn <jerlynrodriguez14@gmail.com>
2026-02-10 14:50:31 -0400

    Proyecto MVC .NET 9 desplegable en Azure App Service

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcAzureUnapec.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
